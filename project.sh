c2da71ce707af296e6a59bf26ae7ab267edb1035
Rub21 <ruben@developmentseed.org>
2022-03-25 15:30:38 -0500

    Add script to start

--- project.sh
+++ project.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
+PROJECT_NAME=$1
+######################################################
+## Clone project
+######################################################
+
+git clone https://github.com/developmentseed/scripts_py_template.git ${PROJECT_NAME}
+
 ######################################################
 ## Rename files
 ######################################################
-mv scripts_py_template ${PROJECT_NAME}
+
 for filename in ./* ./*/*; do
     if [ -f "$filename" ]; then
         echo $filename
-        sed -i -e 's/scripts_py_template/'${PROJECT_NAME}'/g' $filename
+        sed -i -e 's/kiara2/'${PROJECT_NAME}'/g' $filename
+        rm $filename-e
     fi
 done
 
