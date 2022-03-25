b27a449442e23d18ede53aecd86718fa0f0695a0
Rub21 <ruben@developmentseed.org>
2022-03-25 15:30:38 -0500

    Add script to start

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Template to start python script project
 
-check at 👉  [DEV.md](DEV.md)
+This is a template to quickly start a python scripting projects in a docker container. In the data-team we continuously write scripts, we have created this template to standardize our code and follow the best practices in python coding.
+
+## How to install?
+
+Requirements on your operating system:
+
+- [docker](https://www.docker.com/)
+- [ docker-compose](https://docs.docker.com/compose/)
+- [git](https://github.com/git-guides/install-git)
+- [pre-commit](https://pypi.org/project/pre-commit/)
+
+```sh
+export PROJECT_NAME=kiara_project
+curl -s https://raw.githubusercontent.com/developmentseed/scripts_py_template/main/start_py_project.sh | bash /dev/stdin ${PROJECT_NAME}
+```
---
+++ start_py_project.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+#This a script to start a simple Python scripts project with docker eviroment.
+PROJECT_NAME=$1
+######################################################
+## Clone project
+######################################################
+
+git clone https://github.com/developmentseed/scripts_py_template.git ${PROJECT_NAME}
+
+######################################################
+## Rename files
+######################################################
+cd ${PROJECT_NAME}/
+mv scripts_py_template ${PROJECT_NAME}
+for filename in ./* ./*/*; do
+    if [ -f "$filename" ]; then
+        echo $filename
+        sed -i -e 's/scripts_py_template/'${PROJECT_NAME}'/g' $filename
+        if [ -f "$filename-e" ]; then
+            rm $filename-e
+        fi
+    fi
+done
+
+######################################################
+## Create Readme.md
+######################################################
+
+echo "## ${PROJECT_NAME}" >README.md
+echo "" >>README.md
+echo "### Build container and access to Docker environment" >>README.md
+echo "" >>README.md
+echo "\`\`\`sh" >>README.md
+echo "docker-compose build" >>README.md
+echo "docker-compose run ${PROJECT_NAME} bash" >>README.md
+echo "\`\`\`" >>README.md
+
+######################################################
+## Remove unnecessary files
+######################################################
+rm start_py_project.sh
+
+######################################################
+## Build Docker
+######################################################
+docker-compose build
+
+######################################################
+## Github section and linters
+######################################################
+rm -rf .git
+git init
+
+######################################################
+## Github section and linters
+######################################################
+pre-commit install
+pre-commit run --all-files
